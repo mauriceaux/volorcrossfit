0e05590b7dc5f96d9e15e74992484d03fcb9e559
Mauricio Castillo <mauricio.castillo@zeke.cl>
2017-03-15 10:17:25 -0300

    pagina en construccion

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -21,6 +21,7 @@ class Inicio extends CI_Controller {
 	public function index()
 	{
 		$this->load->helper('url');
-		$this->load->view('inicio_vista');
+		//$this->load->view('inicio_vista');
+		$this->load->view('en_construccion');
 	}
 }
